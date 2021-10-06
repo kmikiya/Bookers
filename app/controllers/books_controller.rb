58592f2a337cd7fdc70ea1be1ea0edb5df333214
EC2 Default User <ec2-user@ip-172-31-94-27.ec2.internal>
2021-10-06 08:24:14 +0000

    kadai-syuubann

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,15 +8,30 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save 
-    redirect_to root_path
+    book.save
+    redirect_to book_path(book.id)#ここのbook(モデル).idの意味が分からない。#book(prefix)_path
 
   end
 
   def show
+    @book = Book.find(params[:id])
+
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])#編集されたページのIDが送られてきてそのデータをbookに保存という意味？
+    book.update(book_params)#book_paramsは上じゃダメ？
+    redirect_to book_path(book)#ここのbook(モデル)の意味が分からない idが消えた
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
