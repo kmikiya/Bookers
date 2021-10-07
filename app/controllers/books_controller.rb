d81a81af862a2a926905449c2de2797e793fafab
EC2 Default User <ec2-user@ip-172-31-94-27.ec2.internal>
2021-10-07 03:35:24 +0000

    completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,14 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)#ここのbook(モデル).idの意味が分からない。#book(prefix)_path
-
+    @book = Book.new(book_params)#viewファイルまで飛ばすから@がついてる
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)#ここのbook(モデル).idの意味が分からない。#book(prefix)_path
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -23,14 +27,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])#編集されたページのIDが送られてきてそのデータをbookに保存という意味？
-    book.update(book_params)#book_paramsは上じゃダメ？
-    redirect_to book_path(book)#ここのbook(モデル)の意味が分からない idが消えた
+    @book = Book.find(params[:id])#編集されたページのIDが送られてきてそのデータをbookに保存という意味？
+    if @book.update(book_params)#book_paramsは上じゃダメ？
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)#ここのbook(モデル)の意味が分からない idが消えた
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
